662fcb1394e78d930be2da868ca1ffe026beadd6
Charlene-Lewis8 <charmarlew08@gmail.com>
2026-04-02 16:57:07 -0400

    modal complete

---
+++ app.js
@@ -0,0 +1,33 @@
+let isModalOpen = false;
+
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_mbv83fh',
+            'template_iua9ufq',
+            event.target,
+            'bBfYDTa0sC3C3aeJ9'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact us directly at mopcsmyrna@gmail.com");
+        })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
+
